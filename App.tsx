5736804ee4d968712b9cab553bd9a07634384bce
“ParkChangHee” <arrans9591@gmail.com>
2024-08-22 18:51:53 +0900

    geoloaction 가지고 오기

--- App.tsx
+++ App.tsx
@@ -1,17 +1,41 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
+  const [city, setCity] = useState<null | string>('Loading');
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    // console.log(permission); // {"canAskAgain": true, "expires": "never", "granted": true, "status": "granted"}
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView pagingEnabled horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.weather}>
         <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
+          <Text style={styles.temp}>28</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
         <View style={styles.day}>
